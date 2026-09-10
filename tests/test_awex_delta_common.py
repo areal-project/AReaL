f403694c713a1c4ad5c129be0d067ed8fc029cae
sitabulaixizawaluduo <ljl2020110773@gmail.com>
2026-09-10 16:49:41 +0800

    test(engine): remove unused AWEX peer-transfer stubs

--- tests/test_awex_delta_common.py
+++ tests/test_awex_delta_common.py
@@ -479,7 +479,6 @@ def _load_sglang_adapter(monkeypatch):
     )
 
     nccl_group_mod = types.ModuleType("areal.engine.awex.transport.nccl_group")
-    nccl_group_mod.batch_send_recv_by_peer = lambda **kwargs: None
     nccl_group_mod.init_weights_update_group = lambda *args, **kwargs: None
     nccl_group_mod.setup_batch_isend_irecv = lambda *args, **kwargs: None
     monkeypatch.setitem(
@@ -533,7 +532,6 @@ def _load_megatron_adapter(monkeypatch):
     )
 
     nccl_group_mod = types.ModuleType("areal.engine.awex.transport.nccl_group")
-    nccl_group_mod.batch_send_recv_by_peer = lambda **kwargs: None
     nccl_group_mod.init_weights_update_group = lambda *args, **kwargs: None
     nccl_group_mod.setup_batch_isend_irecv = lambda *args, **kwargs: None
     monkeypatch.setitem(
